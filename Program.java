e3928d33681ff8bbb8f2ecc6e9c9b695dd649647
unknown <inf136793@lab-45-8.cs.put.poznan.pl>
2019-11-06 10:09:16 +0100

    Dodano nową klasę java

---
+++ Program.java
@@ -0,0 +1,33 @@
+public class Samochod {
+
+    private int predkoscMax;
+    private String producent;
+    private String model;
+    private int predkosc;
+
+    public Samochod(String producent, String model, int predkoscMax) {
+        this.predkoscMax = predkoscMax > 0 ? predkoscMax : 0;
+        this.producent = producent != null ? producent : "nieznany";
+        this.model = model != null ? model : "model";
+        this.predkosc = 0;
+    }
+
+    public void przyspiesz(int i) {
+        int nowaPredkosc = this.predkosc + i;
+        this.predkosc = nowaPredkosc < this.predkoscMax ? nowaPredkosc : this.predkoscMax;
+    }
+
+    public void zwolnij(int i) {
+        int nowaPredkosc = this.predkosc - i;
+        this.predkosc = nowaPredkosc > 0 ? nowaPredkosc : 0;
+    }
+
+    public int aktualnaPredkosc() {
+        return this.predkosc;
+    }
+
+    public String toString() {
+        return this.producent + " " + this.model;
+    }
+
+}
